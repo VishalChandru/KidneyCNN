4f89cf3c35ddd77a6a929aa6e2fa287260693095
Vishal Chandru <vishalrocks1998@gmail.com>
2023-11-30 15:17:58 -0600

    model training completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.Kidney_CTScan import logger
 from src.Kidney_CTScan import CustomException
 from src.Kidney_CTScan.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.Kidney_CTScan.pipeline.stage_02_prepare_base_model import PrepareBaseModelPipeline
+from src.Kidney_CTScan.pipeline.stage_03_model_training import TrainingPipepline
 import sys
 
 
@@ -25,3 +26,14 @@ try:
 except Exception as e:
         logger.exception(e)
         raise CustomException(e,sys)
+
+STAGE_NAME = "Model Training"
+try:
+    logger.info(f"********************")
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
+    obj = TrainingPipepline()
+    obj.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise CustomException(e,sys)
